6a0bd330b9528292c0e83b5b3fb64b265c74e89e
Veini Tuominen <veini.tuominen@hotmail.com>
2017-05-12 13:09:45 +0300

    Testing Travis

--- src/test/AppTest.ts
+++ src/test/AppTest.ts
@@ -3,5 +3,6 @@ class AppTest extends UnitTest {
     @Test
     example() {
         this.assertEquals("This text is displayed if 1 doesn't equal 1.", 1, 1);
+        this.fail("This should fail Travis build.");
     }
 }
